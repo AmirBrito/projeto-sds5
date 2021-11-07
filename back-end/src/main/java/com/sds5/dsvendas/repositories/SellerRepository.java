825b8f7c51fa180b6a5215d98608b8f1d4f14812
amirbrito <amir.sbrito@gmail.com>
2021-11-06 22:23:38 -0300

    layers implemented

---
+++ back-end/src/main/java/com/sds5/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.sds5.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sds5.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ back-end/src/main/java/com/sds5/dsvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.sds5.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sds5.dsvendas.dto.SellerDTO;
+import com.sds5.dsvendas.entities.Seller;
+import com.sds5.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
